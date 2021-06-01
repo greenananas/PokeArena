fe47344c380a7021e420093f6914dab2f36cabde
moudjebwork <53222279+moudjebwork@users.noreply.github.com>
2021-06-01 14:55:18 +0200

    Ajout des actions du dresseur sous la nouvelle forme

---
+++ Model/Action.java
@@ -0,0 +1,12 @@
+package Model;
+/**
+ * Interface décrivant le choix d'action du dresseur (Attaque ou changement de Pokémon).
+ */
+public interface Action {
+
+    /**
+     * Type de l'action choisie par le dresseur.
+     */
+    private acTypes type_action;
+
+}
