eca30320dce61bef030e6475d48934f81bfeb2f0
Akodix <44776443+Akodix@users.noreply.github.com>
2021-06-16 15:15:49 +0200

    Javadoc + modif model

--- Model/Battle.java
+++ Model/Battle.java
@@ -4,22 +4,40 @@ package Model;
  * Classe décrivant les actions menées pendant un combat Pokémon.
  */
 public class Battle {
+    /*
+     * Objet représentant le premier dresseur et son action associée.
+     */
     public TrainerAction trainer1;
+    /*
+     * Objet représentant le second dresseur et son action associée.
+     */
     public TrainerAction trainer2;
-
+    /*
+     * Terrain sur lequel a lieu le combat.
+     */
     private static BattleGround bg;  //Terrain de combat
 
+    /**
+     * Défini un combat entre deux dresseurs.
+     *
+     * @param left  Premier dresseur;
+     * @param right Second dresseur.
+     * @param bg    Terrain du combat.
+     */
     public Battle(Trainer left, Trainer right, BattleGround bg) {
         trainer1 = new TrainerAction(left, null);
         trainer2 = new TrainerAction(right, null);
         Battle.bg = bg;
     }
 
-    /*
-     * Actions effectuée pendant un tour de jeu. Utilisée en ligne de commande séquentiel. Non utilisé
+    /**
+     * Applique les actions effectuées pendant un tour de jeu. Utilisée en ligne de commande séquentiel. Non utilisée
      * dans le programme principal.
+     *
+     * @param T1action Action choisie par le premier dresseur.
+     * @param T2action Action choisie par le second dresseur.
      */
-    public void takeTurns (Action T1action, Action T2action) {
+    public void takeTurns(Action T1action, Action T2action) {
         trainer1.pairWith(T1action);
         trainer2.pairWith(T2action);
         TrainerAction firstToAct = calculatePriority();
@@ -36,8 +54,7 @@ public class Battle {
                     bg.applyTrapsEffect(firstToAct.getTrainer().getLeadingPkmn(), target);
                 }
             }
-        }
-        else {
+        } else {
             if (!secondToAct.getTrainer().hasPokemonLeft()) {
                 endBattle(firstToAct.getTrainer());
             } else {
@@ -48,7 +65,14 @@ public class Battle {
         }
     }
 
-    public void apply (TrainerAction current, TrainerAction other, boolean isFirst) {
+    /**
+     * Applique une action spécifiée selon son type.
+     *
+     * @param current Dresseur effectuant l'action.
+     * @param other   Adversaire du dresseur courant.
+     * @param isFirst Est-ce la première action du tour ?
+     */
+    public void apply(TrainerAction current, TrainerAction other, boolean isFirst) {
         if (current.getAction() instanceof Move) {
             Trainer currentTrainer = current.getTrainer();
             Pokemon user = currentTrainer.getLeadingPkmn();
@@ -66,61 +90,71 @@ public class Battle {
                         user.attack(target, move);
                     else
                         applyStatusMoveEffect(move, user, target);
-                }
-                else {
+                } else {
                     System.out.println("\nL'attaque " + move.getName() + " a échoué.");
                 }
                 if (landed) applyPostMoveEffect(move, user, target);
             }
-        }
-        else if (current.getAction() instanceof ChangePkmn) {
+        } else if (current.getAction() instanceof ChangePkmn) {
             current.getTrainer().changePokemon(((ChangePkmn) current.getAction()).getIndex());
             boolean target = (current == trainer1);
             bg.applyTrapsEffect(current.getTrainer().getLeadingPkmn(), target);
         }
     }
 
+    /**
+     * Permet de calculer quel dresseur effectuera son action en premier.
+     *
+     * @return Le joueur ayant le priorité.
+     */
     public TrainerAction calculatePriority() {
         TrainerAction firstToAct;
         applyPrePriority();
 
         if (trainer1.getAction().getPriority() > trainer2.getAction().getPriority()) {
             firstToAct = trainer1;
-        }
-        else if (trainer1.getAction().getPriority() < trainer2.getAction().getPriority()) {
+        } else if (trainer1.getAction().getPriority() < trainer2.getAction().getPriority()) {
             firstToAct = trainer2;
-        }
-        else {
+        } else {
             firstToAct = (trainer1.getTrainer().getLeadingPkmn().isFaster(trainer2.getTrainer().getLeadingPkmn()) ? trainer1 : trainer2);
         }
         return firstToAct;
     }
 
+    /**
+     * Applique des effets d'attaque se passant avant le calcul de priorité.
+     */
     public void applyPrePriority() {
         //Poursuite
         if (trainer1.getAction() instanceof Move && ((Move) trainer1.getAction()).getId() == 228) {
-            if (trainer2.getAction() instanceof ChangePkmn){
+            if (trainer2.getAction() instanceof ChangePkmn) {
                 trainer1.getAction().setPriority(7);
-                ((Move)trainer1.getAction()).setPower(80);
-            }
-            else {
+                ((Move) trainer1.getAction()).setPower(80);
+            } else {
                 trainer1.getAction().setPriority(0);
-                ((Move)trainer1.getAction()).setPower(40);
+                ((Move) trainer1.getAction()).setPower(40);
             }
         }
         if (trainer2.getAction() instanceof Move && ((Move) trainer2.getAction()).getId() == 228) {
             if (trainer1.getAction() instanceof ChangePkmn) {
                 trainer2.getAction().setPriority(7);
-                ((Move)trainer2.getAction()).setPower(80);
-            }
-            else {
+                ((Move) trainer2.getAction()).setPower(80);
+            } else {
                 trainer2.getAction().setPriority(0);
-                ((Move)trainer2.getAction()).setPower(40);
+                ((Move) trainer2.getAction()).setPower(40);
             }
         }
     }
 
-    public void applyPreMoveEffect (Move moveToProcess, TrainerAction current, TrainerAction other, boolean isFirst) {
+    /**
+     * Applique des effets d'attaque ayant lieu avant le calcul des dommages.
+     *
+     * @param moveToProcess Attaque sur le point d'être lancée.
+     * @param current       Dresseur effectuant l'action.
+     * @param other         Adversaire du dresseur courant.
+     * @param isFirst       Est-ce la première action du tour ?
+     */
+    public void applyPreMoveEffect(Move moveToProcess, TrainerAction current, TrainerAction other, boolean isFirst) {
         Trainer currentTrainer = current.getTrainer();
         Pokemon user = currentTrainer.getLeadingPkmn();
         Trainer opponentTrainer = other.getTrainer();
@@ -154,7 +188,14 @@ public class Battle {
         }
     }
 
-    public void applyPostMoveEffect (Move moveToProcess, Pokemon user, Pokemon target) {
+    /**
+     * Applique des effets d'attaque ayant lieu après le calcul des dommages.
+     *
+     * @param moveToProcess Attaque lancée.
+     * @param user          Pokémon effectuant l'attaque.
+     * @param target        Pokémon subissant l'attaque.
+     */
+    public void applyPostMoveEffect(Move moveToProcess, Pokemon user, Pokemon target) {
         switch (moveToProcess.getId()) {
             case 369: //Demi-Tour
                 //getOwner(user).changePokemon();
@@ -163,12 +204,19 @@ public class Battle {
                 if (Math.random() < 0.10) modifyStat(target, "speDefence", -1);
                 break;
             case 413: //Rapace
-                user.getDamaged(user.getMaxHP()/3);
+                user.getDamaged(user.getMaxHP() / 3);
                 break;
         }
     }
 
-    public void applyStatusMoveEffect (Move moveToProcess, Pokemon user, Pokemon target) {
+    /**
+     * Applique les effets des attaques de statut.
+     *
+     * @param moveToProcess Attaque lancée.
+     * @param user          Pokémon effectuant l'attaque.
+     * @param target        Pokémon subissant l'attaque.
+     */
+    public void applyStatusMoveEffect(Move moveToProcess, Pokemon user, Pokemon target) {
         switch (moveToProcess.getId()) {
             case 14: //Danse Lames
                 modifyStat(user, "attack", 2);
@@ -180,33 +228,45 @@ public class Battle {
         }
     }
 
-    public void modifyStat (Pokemon poke, String stat, int value) {
+    /**
+     * Permet de changer le modificateur de statistique d'un pokémon.
+     *
+     * @param poke  Pokémon subissant la modification.
+     * @param stat  Statistique cible de la modification.
+     * @param value Valeur de la modification.
+     */
+    public void modifyStat(Pokemon poke, String stat, int value) {
         switch (stat) {
             case "attack":
-                poke.setAttackModifier(poke.getAttackModifier()+value);
+                poke.setAttackModifier(poke.getAttackModifier() + value);
             case "defence":
-                poke.setDefenceModifier(poke.getDefenceModifier()+value);
+                poke.setDefenceModifier(poke.getDefenceModifier() + value);
             case "speAttack":
-                poke.setSpeAttackModifier(poke.getSpeAttackModifier()+value);
+                poke.setSpeAttackModifier(poke.getSpeAttackModifier() + value);
             case "speDefence":
-                poke.setDefenceModifier(poke.getDefenceModifier()+value);
+                poke.setDefenceModifier(poke.getDefenceModifier() + value);
             case "speed":
-                poke.setSpeedModifier(poke.getSpeedModifier()+value);
+                poke.setSpeedModifier(poke.getSpeedModifier() + value);
         }
     }
 
     /**
-     * L'attaque du Pokémon va-t-elle atteindre sa cible.
+     * L'attaque du Pokémon va-t-elle atteindre sa cible ?
      *
-     * @param target Cible de l'attaque
-     * @param at     Attaque lancée
-     * @return L'attaque lancée touche l'ennemi
+     * @param target Cible de l'attaque.
+     * @param at     Attaque lancée.
+     * @return Si l'attaque lancée touche l'ennemi.
      */
     public boolean doesHit(Pokemon user, Pokemon target, Move at) { //Prendre en compte l'esquive
         int precision = (int) Math.floor(Math.random() * 100);
         return (precision <= at.getPrecision());
     }
 
+    /**
+     * Affiche le vainqueur du combat.
+     *
+     * @param w Dresseur vainqueur du combat.
+     */
     public void endBattle(Trainer w) {
         System.out.println(w.getDisplayName() + " remporte le combat.");
     }
