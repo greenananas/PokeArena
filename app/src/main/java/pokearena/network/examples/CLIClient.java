a40bac694f9e82c1670b0b3344872a62f7139ca1
= <=>
2021-06-27 19:39:03 +0200

    Code refactoring to fix code smells

--- app/src/main/java/pokearena/network/examples/CLIClient.java
+++ app/src/main/java/pokearena/network/examples/CLIClient.java
@@ -15,42 +15,50 @@ import java.util.Scanner;
 public class CLIClient {
 
     public static void main(String[] args) {
-        PokeArenaClient client;
+
+        // Initialisation d'objets pour l'exemple
+        var dracocharge = new Move(1, "Dracocharge", PokeTypes.type.DRAGON, true, 100, 70, 16, 0, 0);
+        var dracogriffe = new Move(2, "Dracogriffe", PokeTypes.type.DRAGON, true, 80, 100, 24, 0, 0);
+        var seisme = new Move(3, "Séisme", PokeTypes.type.GROUND, true, 100, 100, 16, 0, 0);
+        var lanceflammes = new Move(4, "Lance-Flammes", PokeTypes.type.FIRE, false, 90, 100, 24, 0, 0);
+
+        var poingmeteor = new Move(4, "Poing Météore", PokeTypes.type.STEEL, true, 90, 90, 16, 0, 0);
+        var poingglace = new Move(5, "Poing Glace", PokeTypes.type.ICE, true, 75, 100, 24, 0, 0);
+        var poingeclair = new Move(6, "Poing Éclair", PokeTypes.type.ELECTRIC, true, 75, 100, 24, 0, 0);
+
+        var lamederock = new Move(7, "Lame de Roc", PokeTypes.type.ROCK, true, 100, 80, 8, 0, 0);
+        var poingfeu = new Move(8, "Poing Feu", PokeTypes.type.FIRE, true, 75, 100, 24, 0, 0);
+        var machouille = new Move(9, "Machouille", PokeTypes.type.DARK, true, 80, 100, 24, 0, 0);
+
+        var lamedair = new Move(10, "Lame d'Air", PokeTypes.type.FLYING, false, 75, 95, 24, 0, 0);
+        var eclatmagique = new Move(11, "Éclat Magique", PokeTypes.type.FAIRY, false, 80, 100, 16, 0, 0);
+        var ballombre = new Move(12, "Ball'Ombre", PokeTypes.type.GHOST, false, 80, 100, 24, 0, 0);
+
+        var surf = new Move(13, "Surf", PokeTypes.type.WATER, false, 95, 100, 24, 0, 0);
+        var hydrocanon = new Move(14, "Hydrocanon", PokeTypes.type.WATER, false, 120, 80, 8, 0, 0);
+        var luminocanon = new Move(15, "Luminocanon", PokeTypes.type.STEEL, false, 80, 100, 16, 0, 0);
+        var laserglace = new Move(16, "Laser Glace", PokeTypes.type.ICE, false, 95, 100, 16, 0, 0);
+
+        var nature = "HARDY";
+        var carchacrok = new Pokemon("Carchacrok", PokeTypes.type.DRAGON, PokeTypes.type.GROUND, 50, 108, 130, 95, 80, 85, 102, 0, 0, 0, 0, 0, 0, nature, dracocharge, dracogriffe, seisme, lanceflammes, 445);
+        var metalosse = new Pokemon("Metalosse", PokeTypes.type.STEEL, PokeTypes.type.PSYCHIC, 50, 80, 135, 130, 95, 90, 70, 0, 0, 0, 0, 0, 0, nature, poingmeteor, seisme, poingglace, poingeclair, 376);
+        var tyranocif = new Pokemon("Tyranocif", PokeTypes.type.ROCK, PokeTypes.type.DARK, 50, 100, 134, 110, 95, 100, 61, 0, 0, 0, 0, 0, 0, nature, lamederock, poingfeu, seisme, machouille, 248);
+        var togekiss = new Pokemon("Togekiss", PokeTypes.type.FAIRY, PokeTypes.type.FLYING, 50, 85, 50, 95, 120, 115, 80, 0, 0, 0, 0, 0, 0, nature, lamedair, eclatmagique, ballombre, lanceflammes, 468);
+        var elekable = new Pokemon("Elekable", PokeTypes.type.ELECTRIC, null, 50, 75, 123, 67, 95, 85, 95, 0, 0, 0, 0, 0, 0, nature, poingeclair, poingfeu, poingglace, seisme, 446);
+        var pingoleon = new Pokemon("Pingoléon", PokeTypes.type.WATER, PokeTypes.type.STEEL, 50, 84, 86, 88, 111, 101, 60, 0, 0, 0, 0, 0, 0, nature, surf, hydrocanon, luminocanon, laserglace, 395);
+
+        PokeArenaClient client = null;
+
         try {
             client = new PokeArenaClient("localhost", 8887);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        if (client != null) {
             client.connect();
             var sc = new Scanner(System.in);
             String input = sc.next();
-
-            // Initialisation d'objets pour l'exemple
-            var dracocharge = new Move(1,"Dracocharge", PokeTypes.type.DRAGON, true, 100, 70, 16, 0, 0);
-            var dracogriffe = new Move(2,"Dracogriffe", PokeTypes.type.DRAGON, true, 80, 100, 24, 0, 0);
-            var seisme = new Move(3,"Séisme", PokeTypes.type.GROUND, true, 100, 100, 16, 0, 0);
-            var lanceflammes = new Move(4,"Lance-Flammes", PokeTypes.type.FIRE, false, 90, 100, 24, 0, 0);
-
-            var poingmeteor = new Move(4,"Poing Météore", PokeTypes.type.STEEL, true, 90, 90, 16, 0, 0);
-            var poingglace = new Move(5,"Poing Glace", PokeTypes.type.ICE, true, 75, 100, 24, 0, 0);
-            var poingeclair = new Move(6,"Poing Éclair", PokeTypes.type.ELECTRIC, true, 75, 100, 24, 0, 0);
-
-            var lamederock = new Move(7,"Lame de Roc", PokeTypes.type.ROCK, true, 100, 80, 8, 0, 0);
-            var poingfeu = new Move(8,"Poing Feu", PokeTypes.type.FIRE, true, 75, 100, 24, 0, 0);
-            var machouille = new Move(9,"Machouille", PokeTypes.type.DARK, true, 80, 100, 24, 0, 0);
-
-            var lamedair = new Move(10,"Lame d'Air", PokeTypes.type.FLYING, false, 75, 95, 24, 0, 0);
-            var eclatmagique = new Move(11,"Éclat Magique", PokeTypes.type.FAIRY, false, 80, 100, 16, 0, 0);
-            var ballombre = new Move(12,"Ball'Ombre", PokeTypes.type.GHOST, false, 80, 100, 24, 0, 0);
-
-            var surf = new Move(13,"Surf", PokeTypes.type.WATER, false, 95, 100, 24, 0, 0);
-            var hydrocanon = new Move(14,"Hydrocanon", PokeTypes.type.WATER, false, 120, 80, 8, 0, 0);
-            var luminocanon = new Move(15,"Luminocanon", PokeTypes.type.STEEL, false, 80, 100, 16, 0, 0);
-            var laserglace = new Move(16,"Laser Glace", PokeTypes.type.ICE, false, 95, 100, 16, 0, 0);
-
-            var carchacrok = new Pokemon("Carchacrok", PokeTypes.type.DRAGON, PokeTypes.type.GROUND, 50, 108, 130, 95, 80, 85, 102,0,0,0,0,0,0,"HARDY", dracocharge, dracogriffe, seisme, lanceflammes, 445);
-            var metalosse = new Pokemon("Metalosse", PokeTypes.type.STEEL, PokeTypes.type.PSYCHIC, 50, 80, 135, 130, 95, 90, 70,0,0,0,0,0,0,"HARDY", poingmeteor, seisme, poingglace, poingeclair, 376);
-            var tyranocif = new Pokemon("Tyranocif", PokeTypes.type.ROCK, PokeTypes.type.DARK, 50, 100, 134, 110, 95, 100, 61,0,0,0,0,0,0,"HARDY", lamederock, poingfeu, seisme, machouille, 248);
-            var togekiss = new Pokemon("Togekiss", PokeTypes.type.FAIRY, PokeTypes.type.FLYING, 50, 85, 50, 95, 120, 115, 80,0,0,0,0,0,0,"HARDY", lamedair, eclatmagique, ballombre, lanceflammes, 468);
-            var elekable = new Pokemon("Elekable", PokeTypes.type.ELECTRIC, null, 50, 75, 123, 67, 95, 85, 95,0,0,0,0,0,0,"HARDY", poingeclair, poingfeu, poingglace, seisme, 446);
-            var pingoleon = new Pokemon("Pingoléon", PokeTypes.type.WATER, PokeTypes.type.STEEL, 50, 84, 86, 88, 111, 101, 60,0,0,0,0,0,0,"HARDY", surf, hydrocanon, luminocanon, laserglace, 395);
 
             ArrayList<Pokemon> alTeam1 = new ArrayList<>();
             alTeam1.add(carchacrok);
@@ -171,8 +179,6 @@ public class CLIClient {
                 input = sc.next();
             }
             client.close();
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
         }
     }
 }
